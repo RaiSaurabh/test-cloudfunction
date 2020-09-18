9b9d3b6f3750c58eca23663270b48d98d682d9d6
Saurabh Rai <rai.saurabh@hotmail.com>
2020-09-18 10:58:42 +0530

    updated

---
+++ src/main/java/com/redhat/testcloudfunction/TestCloudfunctionApplication.java
@@ -0,0 +1,32 @@
+package com.redhat.testcloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class TestCloudfunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCloudfunctionApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> sendandreceive() {
+		return input -> input;
+	}
+
+	@Bean
+	public Consumer<String> send() {
+		return input -> System.out.println("Input Received is: " + input);
+	}
+
+	@Bean
+	public Supplier<String> hello() {
+		return () -> "Hello There......";
+	}
+}
---
+++ src/test/java/com/redhat/testcloudfunction/TestCloudfunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.testcloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCloudfunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
